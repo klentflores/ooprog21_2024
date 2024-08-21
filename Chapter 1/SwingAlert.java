2e21b6eb8eb44b7e3e1b1d08a45af2d520171f7b
Klent Flores <klentflores25@gmail.com>
2024-08-21 19:04:30 +0800

    Create  SwingAlert.java

---
+++ Chapter 1/SwingAlert.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class SwingAlert{
+
+   public static void main(String[] args){
+   
+   JOptionPane.showMessageDialog(null, "Hello World!");
+   
+   }
+ }
